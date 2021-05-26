ca10705ecb36a327b029e15803b40b045e2ea24e
SSABOODA <69753846+SSABOODA@users.noreply.github.com>
2021-05-26 12:40:31 +0900

    1번 과제 완료

---
+++ owners/views.py
@@ -0,0 +1,69 @@
+import json
+
+from django.db.models.fields.related import ForeignKey
+from django.http import JsonResponse
+from django.views import View
+from .models import Owner, Dog
+
+
+class OwnerListView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        Owner.objects.create(name=data['name'],
+                             email=data['email'],
+                             age=data['age'])
+
+        return JsonResponse({'MESSAGE': 'SUCCESS'}, status=201)
+
+    def get(self, request):
+
+        owners = Owner.objects.all()
+        result = []
+        for owner in owners:
+            value = owner.dog_set.all()
+            dog_result = []
+
+            for dog in value:
+                dog_info = {
+                    'name': dog.name,
+                    'age': dog.age
+                }
+            dog_result.append(dog_info)
+            result.append(
+                {
+                    'name': owner.name,
+                    'age': owner.age,
+                    'email': owner.email,
+                    'my dog': dog_result
+                }
+            )
+
+        return JsonResponse({'result': result}, status=200)
+
+        ############################################################################################################################################################################################
+
+
+class DogListView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        Dog.objects.create(name=data['name'],
+                           age=data['age'],
+                           owner=Owner.objects.get(name=data['owner'])
+                           )
+
+        return JsonResponse({'MESSAGE': 'SUCCESS'}, status=201)
+
+    def get(self, request):
+        dogs = Dog.objects.all()
+
+        result = []
+        for dog in dogs:
+            result.append(
+                {
+                    'name': dog.name,
+                    'age': dog.age,
+                    'master': dog.owner.name
+                }
+            )
+
+        return JsonResponse({'MESSAGE': result}, status=200)
